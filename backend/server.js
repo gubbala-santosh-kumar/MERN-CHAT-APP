cd296c160579a188500bc23fb2b628a3350b6208
git <gubbalasantoshkumar2004@gmail.com>
2025-05-28 21:35:00 +0530

    Chat App

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data.js");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+  res.send(chats);
+  console.log(chats);
+  console.log(req.params.id);
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  res.send(req);
+  console.log(req.params.id);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log("Server Started on PORT 3000"));
